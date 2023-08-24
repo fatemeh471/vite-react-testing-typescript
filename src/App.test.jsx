4b46576c78362a84f70ad68d626f0b7e343fded6
fatemeh471 <54301680+fatemeh471@users.noreply.github.com>
2023-08-25 00:11:02 +0330

    first commit

---
+++ src/App.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {describe, expect, it, test} from 'vitest'
+import App from "./App";
+
+describe("Simple working test", () => {
+	test("the title is visible", () => {
+		render(<App />);
+		expect(screen.getByText("Vite + React")).toBeInTheDocument();
+	});
+
+	it("should increment count on click", async () => {
+		render(<App />);
+		const counter = screen.getByRole("button", { name: /count is/i });
+		expect(counter.textContent).toBe("count is 0");
+		await userEvent.click(counter);
+		expect(counter.textContent).toBe("count is 1");
+	});
+});
